b6de5b0972cd216cd787ee9b72e4e57ff1172693
Ishar-Menon <isharmenon99@gmail.com>
2020-04-25 02:17:07 +0530

    completed insert with rebuild

---
+++ src/client.cpp
@@ -0,0 +1,18 @@
+#include "scapeGoatTree.hpp"
+
+int main()
+{
+    scapeGoatTree tree;
+    tree.insert(10);
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(8);
+    tree.insert(4);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(5);
+    tree.insert(9);
+    tree.insert(6);
+    tree.display();
+    return 0;
+}
